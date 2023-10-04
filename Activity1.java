741fe869b7fa3a37dd96e3edfcc75cded3821e82
Aliahmad2412 <aliahmad12200124@gmail.com>
2023-10-04 11:31:04 +0500

    FirstFile

---
+++ Activity1.java
@@ -0,0 +1,18 @@
+public class Activity1
+{
+	public static void main(String[]args)
+	{
+	int k,l;
+		for( k=5;k<=1;k--)
+		{
+
+			 for ( l=1; l>=k;l++)
+			{
+			
+		     	System.out.println("* ");
+				
+	        }
+	               System.out.println();//new line	
+		}
+	}
+}
